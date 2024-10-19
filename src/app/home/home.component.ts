48dd748861fdf096c10c60c69aa0ab275f218665
Raquel Illerias <raquel.illerias@gmail.com>
2024-10-19 14:30:36 +0100

    add paginator component. Add animation to the paginator, so user can choose the amount of products per page is displayed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,15 +17,27 @@ export class HomeComponent {
 
   products: Product[] = [];
 
+  totalRecords: number = 0;
+  rows: number = 5;
+
   onProductOutput(product: Product) {
     console.log(product, 'Output');
   }
 
-  ngOnInit() {
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
     this.productsService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
       .subscribe((products: Products) => {
         this.products = products.items;
+        this.totalRecords = products.total;
       });
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 }
