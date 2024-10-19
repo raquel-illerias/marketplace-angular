f2d48e08633f1d94ffa7eedb55a80ced4e707248
Raquel Illerias <raquel.illerias@gmail.com>
2024-10-19 14:18:13 +0100

    display and style product images, name, price and rating

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -15,6 +16,10 @@ export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
   products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
 
   ngOnInit() {
     this.productsService
